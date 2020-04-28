86a47ea6bdd2a022edff70dab4628e3bb02adf58
droogadulce <ahigareda.mayra@ciencias.unam.mx>
2020-04-27 22:16:17 -0500

    Validations

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,20 +3,32 @@
 namespace App\Controllers;
     
 use App\Models\Job;
+use Respect\Validation\Validator as v;
 
 class JobsController extends BaseController {
 
     public function getAddJobAction($request) {
+        $responseMessage = '';
         if ($request->getMethod() == 'POST') {
             $postData = $request->getParsedBody();
-            $job = new Job();
-            $job->title = $postData['title'];
-            $job->description = $postData['description'];
-            $job->visible = true;
-            $job->months = 3;
-            $job->save();
+            $jobValidator = v::key('title', v::stringType()->notEmpty())
+                            ->key('description', v::stringType()->notEmpty());
+            try {
+                $jobValidator->assert($postData);
+                $job = new Job();
+                $job->title = $postData['title'];
+                $job->description = $postData['description'];
+                $job->visible = true;
+                $job->months = 3;
+                $job->save();
+                $responseMessage = 'Saved';
+            } catch (\Exception $e) {
+                $responseMessage = $e->getMessage();
+            }
         }
 
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig', [
+            'responseMessage' => $responseMessage
+        ]);
     }
 }
